35f998b7c93ccb026dbd74877b165951d0313c7e
9527 <luxiaohu@hotmail.com>
2017-05-26 02:09:49 -0400

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # DevOps-Python
+第一次使用
+git init
+mkdir woniu
+echo  print 123 >> woniu/zuoye.py
+git add .
+git commit -m "first commit"
+git remote add origin https://github.com/51reboot/actual-12-homework.git
+git push -u origin master
+
+后面添加代码，只需要下面三行即可
+git add .
+git commit -m "first commit"
+git push -u origin master
+
---
+++ homework/Day01_homework.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Author: anchen
+# @Date:   2017-05-16 16:40:21
+# @Last Modified by:   anchen
+# @Last Modified time: 2017-05-25 16:27:5
+# 
+# 
+# 
+num = 33.3
+print num
+
+print "this nums are %f and %f" % (num,-3.1)
+print "this nums are %f and %f" % (num,-3.1)
+print "this nums are %f and %f" % (num,-3.1)
+print "this nums are %f and %f" % (num,-3.1)
+
+ddd = {'aa':31,'bb':55}
+print "xxxxxx %(aa)s " % ddd
+
+def mwbyd(name):
+    print mwbyd
+    callable(mwbyd)
+
+    print mwbyd(lu)
+
